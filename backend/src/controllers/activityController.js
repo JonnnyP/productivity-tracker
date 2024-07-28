17ce8ce266f4fa4ae9cea30aa44339244e129643
JonnnyP <jonathan_plata@ymail.com>
2024-07-27 19:05:45 -0700

    activity routes added

--- backend/src/controllers/activityController.js
+++ backend/src/controllers/activityController.js
@@ -33,7 +33,59 @@ const getAllActivities = async (req,res) => {
     }
 }
 
+const getSpecificUserActivities = async (req,res) => {
+    try {
+        const { userId } = req.params
+
+        const activities = await Activity.findAll({
+            where:  { userId},
+            include: [{
+                model: User,
+                attributes: ['id','username','email']
+            }]
+        })
+
+        if ( activities.length === 0 ) {
+            return res.status(404).json({ message: 'No activities found for this user'})
+        }
+
+        res.status(200).json(activities)
+    } catch (err) {
+        console.log('Error retrieving activities for user:', err);
+        res.status(500).json({error: err.message})
+    }
+}
+
+const updateActivity = async (req,res) => {
+    try {
+        const { id } = req.params
+        const {type, description, duration} = req.body
+
+        const activity = await Activity.findByPk(id)
+
+        if(!activity) {
+            return res.status(404).json({error: 'Activity not found'})
+        }
+
+        if (type && !['productive', 'exercise', 'hobby'].includes(type)) {
+            return res.status(400).json({ error: 'Invalid type' });
+        }
+        
+        activity.type = type || activity.type;
+        activity.description = description || activity.description;
+        activity.duration = duration || activity.duration;
+    
+        await activity.save();
+    
+        res.status(200).json(activity);
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
 module.exports = {
     createActivity,
     getAllActivities,
+    getSpecificUserActivities,
+    updateActivity,
 }
--- backend/src/routes/activityRoutes.js
+++ backend/src/routes/activityRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const activityController = require('../controllers/activityController')
 
 router.post('/createNewActivity', activityController.createActivity)
+router.get('/user/:userId', activityController.getSpecificUserActivities)
 router.get('/', activityController.getAllActivities)
+router.put('/activities/;id', activityController.updateActivity)
 
 module.exports = router
